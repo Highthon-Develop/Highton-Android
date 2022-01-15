730f2acc8dd14d1b89e0247fe8c0c81e4d3281ae
ckrudals <2004ckck@naver.com>
2022-01-15 22:13:21 +0900

    update BaseActivity.kt

--- app/src/main/java/com/example/highton_android/base/BaseActivity.kt
+++ app/src/main/java/com/example/highton_android/base/BaseActivity.kt
@@ -1,14 +1,27 @@
 package com.example.highton_android.base
 
+import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
-open class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val contentLayoutId: Int) :
+    AppCompatActivity() {
 
-    protected inline fun <reified T : ViewDataBinding> binding(@LayoutRes resId: Int): Lazy<T> =
-        lazy { DataBindingUtil.setContentView<T>(this, resId) }
+
+    lateinit var binding: T
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, contentLayoutId)
+
+
+    }
+    protected fun makeToast(msg: String) = Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 
 
 }
