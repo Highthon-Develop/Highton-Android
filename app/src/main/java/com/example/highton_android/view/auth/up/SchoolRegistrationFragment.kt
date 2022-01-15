d071a80f9c6ef0ceec9c3fbbf2d74f487634b6ac
ckrudals <2004ckck@naver.com>
2022-01-16 03:28:03 +0900

    init

---
+++ app/src/main/java/com/example/highton_android/view/auth/up/SchoolRegistrationFragment.kt
@@ -0,0 +1,24 @@
+package com.example.highton_android.view.auth.up
+
+import androidx.navigation.fragment.findNavController
+import com.example.highton_android.R
+import com.example.highton_android.base.BaseFragment
+import com.example.highton_android.databinding.FargmentAuthSchoolRegistrationBinding
+
+class SchoolRegistrationFragment :
+    BaseFragment<FargmentAuthSchoolRegistrationBinding>(R.layout.fargment_auth_school_registration) {
+    override fun FargmentAuthSchoolRegistrationBinding.onCreateView() {
+
+    }
+
+    override fun FargmentAuthSchoolRegistrationBinding.onViewCreated() {
+        with(binding) {
+            searchSchoolLayout.setOnClickListener {
+                findNavController().navigate(R.id.action_schoolRegistrationFragment_to_authStudentInFormationFragment)
+            }
+            inviteCodeLayout.setOnClickListener {
+                //todo
+            }
+        }
+    }
+}
