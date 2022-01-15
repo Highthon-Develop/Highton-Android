d4623be82281899a4c00a4167298e82e3986299c
양태웅 <didxodnd0422@gmail.com>
2022-01-15 22:31:56 +0900

    Apply DataBinding in MainActivity

--- app/src/main/java/com/example/highton_android/view/MainActivity.kt
+++ app/src/main/java/com/example/highton_android/view/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.highton_android.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.highton_android.R
+import com.example.highton_android.base.BaseActivity
+import com.example.highton_android.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
 
     }
 }
