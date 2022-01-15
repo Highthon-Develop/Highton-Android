92355554eb3f01ffb5d9228ae5ba38de2f0f3acd
양태웅 <didxodnd0422@gmail.com>
2022-01-15 16:16:22 +0900

    Add AuthActivity

---
+++ app/src/main/java/com/example/highton_android/view/auth/AuthActivity.kt
@@ -0,0 +1,16 @@
+package com.example.highton_android.view.auth
+
+import android.os.Bundle
+import com.example.highton_android.R
+import com.example.highton_android.base.BaseActivity
+import com.example.highton_android.databinding.ActivityAuthBinding
+
+class AuthActivity : BaseActivity() {
+    private val binding = binding<ActivityAuthBinding>(R.layout.activity_auth)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
