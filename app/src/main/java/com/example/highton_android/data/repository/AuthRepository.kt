ffbe87e0f69e6cadced99e0f29c17f911338967b
ckrudals <2004ckck@naver.com>
2022-01-15 21:42:38 +0900

    add login,account business logic

--- app/src/main/java/com/example/highton_android/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/highton_android/data/repository/AuthRepository.kt
@@ -1,12 +1,34 @@
 package com.example.highton_android.data.repository
 
 import com.example.highton_android.data.TokenDataStore
+import com.example.highton_android.data.model.auth.request.AccountRequest
+import com.example.highton_android.data.model.auth.request.LoginRequest
+import com.example.highton_android.data.model.auth.response.AccountResponse
+import com.example.highton_android.data.model.auth.response.LoginResponse
+import com.example.highton_android.data.service.AuthService
+import retrofit2.Response
 import javax.inject.Inject
 
-class AuthRepository @Inject constructor(private val dataStore: TokenDataStore) {
+class AuthRepository @Inject constructor(
+    private val dataStore: TokenDataStore,
+    private val authApi: AuthService
+) {
 
     var readToken = dataStore.readToken
 
     suspend fun saveToken(token: String) =
         dataStore.saveToken(token)
+
+    suspend fun postAccount(
+        body: AccountRequest
+    ): Response<AccountResponse> {
+        return authApi.postAccount(body = body)
+    }
+
+    suspend fun postLogin(
+        body: LoginRequest
+    ): Response<LoginResponse> {
+        return authApi.postLogin(body = body)
+    }
+
 }
