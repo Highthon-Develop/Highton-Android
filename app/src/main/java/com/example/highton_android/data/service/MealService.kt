8322e0a7a514197a9e108270db52421600ea6087
양태웅 <didxodnd0422@gmail.com>
2022-01-15 21:12:07 +0900

    Add MealService

---
+++ app/src/main/java/com/example/highton_android/data/service/MealService.kt
@@ -0,0 +1,11 @@
+package com.example.highton_android.data.service
+
+import com.example.mealgo.data.meal.model.MealResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface MealService {
+    @GET("mealServiceDietInfo")
+    suspend fun getMealList(@QueryMap queries: Map<String, String>): Response<MealResponse>
+}
