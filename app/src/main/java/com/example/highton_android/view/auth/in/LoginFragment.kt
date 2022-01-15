7ecafeacc5accc48365daebb74d15c60ca48f1d1
ckrudals <2004ckck@naver.com>
2022-01-16 03:26:30 +0900

    add auth layouts

---
+++ app/src/main/java/com/example/highton_android/view/auth/in/LoginFragment.kt
@@ -0,0 +1,61 @@
+package com.example.highton_android.view.auth.`in`
+
+import android.text.TextUtils
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.highton_android.R
+import com.example.highton_android.base.BaseFragment
+import com.example.highton_android.data.model.auth.request.LoginRequest
+import com.example.highton_android.databinding.FragmentAuthLoginBinding
+import com.example.highton_android.viewmodel.auth.AuthViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class LoginFragment : BaseFragment<FragmentAuthLoginBinding>(R.layout.fragment_auth_login) {
+
+    private val viewModel: AuthViewModel by viewModels()
+
+
+    override fun FragmentAuthLoginBinding.onCreateView() {
+
+
+    }
+
+    override fun FragmentAuthLoginBinding.onViewCreated() {
+        with(viewModel) {
+            binding.button.setOnClickListener {
+                lifecycleScope.launch {
+                    if (textNullCheck()) {
+                        postLogin(
+                            LoginRequest(
+                                binding.idEditText.text.toString(),
+                                binding.passwordEditText.text.toString()
+                            )
+                        )
+                    } else {
+                        Toast.makeText(requireContext(), "빈칸을 입력해 주세요!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            isSuccess.observe(viewLifecycleOwner) {
+               if(it){
+                   //TODO
+               }else{
+                   Toast.makeText(requireContext(), "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+               }
+            }
+            isFailure.observe(viewLifecycleOwner) {
+                Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun textNullCheck(): Boolean {
+
+        return !(TextUtils.isEmpty((binding.idEditText.text.toString()))) && !(TextUtils.isEmpty((binding.passwordEditText.text.toString())))
+
+    }
+}
