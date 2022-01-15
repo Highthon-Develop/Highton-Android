fa0748b70c7345f3bd249f0574ead90922cd515f
ckrudals <2004ckck@naver.com>
2022-01-16 07:53:16 +0900

    init

--- app/src/main/java/com/example/highton_android/di/RepositoryModule.kt
+++ app/src/main/java/com/example/highton_android/di/RepositoryModule.kt
@@ -3,8 +3,10 @@ package com.example.highton_android.di
 import com.example.highton_android.data.TokenDataStore
 import com.example.highton_android.data.repository.AuthRepository
 import com.example.highton_android.data.repository.DiaryRepository
+import com.example.highton_android.data.repository.FeedRepository
 import com.example.highton_android.data.service.AuthService
 import com.example.highton_android.data.service.DiaryService
+import com.example.highton_android.data.service.FeedService
 import com.example.highton_android.data.service.SearchSchoolService
 import dagger.Module
 import dagger.Provides
@@ -23,7 +25,7 @@ object RepositoryModule {
     @Singleton
     fun provideAuthRepository(dataStore:TokenDataStore,api:AuthService,searchSchoolApi:SearchSchoolService)=AuthRepository(dataStore,api,searchSchoolApi)
 
-//    @Provides
-//    @Singleton
-//    fun provideFeedRepository(api:DiaryService)=DiaryRepository(api)
+    @Provides
+    @Singleton
+    fun provideFeedRepository(api:FeedService)=FeedRepository(api)
 }
