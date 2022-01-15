afa586ba37289cb2ce898ac9ca4059cd374091ea
ckrudals <2004ckck@naver.com>
2022-01-16 03:27:33 +0900

    add repository Module

--- app/src/main/java/com/example/highton_android/di/RepositoryModule.kt
+++ app/src/main/java/com/example/highton_android/di/RepositoryModule.kt
@@ -1,5 +1,21 @@
 package com.example.highton_android.di
 
+import com.example.highton_android.data.TokenDataStore
+import com.example.highton_android.data.repository.AuthRepository
+import com.example.highton_android.data.service.AuthService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(dataStore : TokenDataStore,authApi:AuthService)=AuthRepository(dataStore,authApi)
+
 
 }
