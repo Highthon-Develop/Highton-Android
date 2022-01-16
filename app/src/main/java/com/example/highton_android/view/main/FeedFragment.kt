43240ab3aec9e458bad629230c8e56b6fcc37f7e
ckrudals <2004ckck@naver.com>
2022-01-16 13:04:34 +0900

    add feed , feed infor layout

--- app/src/main/java/com/example/highton_android/view/main/FeedFragment.kt
+++ app/src/main/java/com/example/highton_android/view/main/FeedFragment.kt
@@ -1,5 +1,6 @@
 package com.example.highton_android.view.main
 
+import androidx.navigation.fragment.findNavController
 import com.example.highton_android.R
 import com.example.highton_android.base.BaseFragment
 import com.example.highton_android.databinding.FragmentFeedBinding
@@ -8,5 +9,9 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 
 class FeedFragment : BaseFragment<FragmentFeedBinding>(R.layout.fragment_feed) {
-
+    override fun FragmentFeedBinding.onViewCreated() {
+        binding.imageView.setOnClickListener {
+            findNavController().navigate(R.id.action_feedFragment_to_feedInforFragment)
+        }
+    }
 }
---
+++ app/src/main/java/com/example/highton_android/view/main/FeedInforFragment.kt
@@ -0,0 +1,8 @@
+package com.example.highton_android.view.main
+
+import com.example.highton_android.R
+import com.example.highton_android.base.BaseFragment
+import com.example.highton_android.databinding.FragmentFeedInforBinding
+
+class FeedInforFragment : BaseFragment<FragmentFeedInforBinding>(R.layout.fragment_feed_infor) {
+}
